b57e03cc1a009e2e1c2d45cdee87a2a02b254d41
Henri Joël Aïdasso <ahenrij@gmail.com>
2019-12-23 19:16:10 +0100

    models created

---
+++ app/Http/Controllers/WishBoxController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\WishBox;
+use Illuminate\Http\Request;
+
+class WishBoxController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\WishBox  $wishBox
+     * @return \Illuminate\Http\Response
+     */
+    public function show(WishBox $wishBox)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\WishBox  $wishBox
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(WishBox $wishBox)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\WishBox  $wishBox
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, WishBox $wishBox)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\WishBox  $wishBox
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(WishBox $wishBox)
+    {
+        //
+    }
+}
---
+++ database/factories/WishBoxFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\WishBox;
+use Faker\Generator as Faker;
+
+$factory->define(WishBox::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
