62cbd742a6f8f49978c3550a6a7021f34b786f40
Henri Joël Aïdasso <ahenrij@gmail.com>
2019-12-26 19:44:39 +0100

    views init

---
+++ resources/views/users/profile.blade.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by TheFather <ahenrij@gmail.com>.
+ * User: HD
+ * Date: 26/12/2019
+ * Time: 19:06
+ */
---
+++ resources/views/wishbox/index.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container-fluid">
+        <h3 class="text-center">Contenu</h3>
+    </div>
+@endsection
