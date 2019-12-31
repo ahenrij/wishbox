208f24668a819300d20301b0705251c6fdf36c3b
GQuenot <gurvan.quenot@gmail.com>
2019-12-31 17:02:47 +0100

    Style Auth
    Changement graphique du login et traduction des pages de login / register

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,72 +1,69 @@
 @extends('layouts.app')
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-6">
-            <form method="POST" action="{{ route('login') }}">
-                @csrf
-                <div class="form-group">
-                    <input
-                        type="text"
-                        id="email"
-                        name="email"
-                        class="form-control rounded-0 border-top-0 border-right-0 border-left-0 @error('email') is-invalid @enderror"
-                        value="{{ old('email') }}"
-                        placeholder="E-mail"
-                        required autofocus
-                        autocomplete="email"/>
-                    @error('email')
-                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                    @enderror
-                </div>
-                <div class="form-group">
-                    <input
-                        type="password"
-                        id="password"
-                        name="password"
-                        class="form-control rounded-0 border-top-0 border-right-0 border-left-0 @error('password') is-invalid @enderror"
-                        value="{{ old('email') }}"
-                        placeholder="mot de passe"
-                        required autofocus
-                        autocomplete="password"/>
-                    @error('password')
-                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                    @enderror
-                </div>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">{{ __('Connexion') }}</div>
+                    <div class="card-body">
+                        <form method="POST" action="{{ route('login') }}" class="form-connect">
+                            @csrf
+                            <div class="form-group">
+                                <input
+                                    type="text"
+                                    id="email"
+                                    name="email"
+                                    class="form-control border-top-0 border-right-0 border-left-0 @error('email') is-invalid @enderror"
+                                    value="{{ old('email') }}"
+                                    placeholder="E-mail"
+                                    required autofocus
+                                    autocomplete="email"/>
+                                @error('email')
+                                <span class="invalid-feedback" role="alert">
+                                                <strong>{{ $message }}</strong>
+                                            </span>
+                                @enderror
+                            </div>
+                            <div class="form-group">
+                                <input
+                                    type="password"
+                                    id="password"
+                                    name="password"
+                                    class="form-control border-top-0 border-right-0 border-left-0 @error('password') is-invalid @enderror"
+                                    value="{{ old('email') }}"
+                                    placeholder="mot de passe"
+                                    required autofocus
+                                    autocomplete="password"/>
+                                @error('password')
+                                <span class="invalid-feedback" role="alert">
+                                                <strong>{{ $message }}</strong>
+                                            </span>
+                                @enderror
+                            </div>
 
-                <div class="form-group">
-                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
+                            <div class="form-group">
+                                <input class="form-check-input ml-1" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
 
-                    <label class="form-check-label" for="remember">
-                        {{ __('Remember Me') }}
-                    </label>
-                </div>
+                                <label class="form-check-label ml-4" for="remember">
+                                    {{ __('Se souvenir de moi') }}
+                                </label>
+                            </div>
 
-                <div class="form-group">
-                        <button type="submit" class="btn btn-secondary tm-btn-submit rounded-0">
-                            {{ __('Connexion') }}
-                        </button>
+                            <div class="form-group">
+                                <button type="submit" class="btn btn-primary">
+                                    {{ __('Connexion') }}
+                                </button>
 
-                        @if (Route::has('password.request'))
-                            <a class="btn btn-link" href="{{ route('password.request') }}">
-                                {{ __('Mot de passe oublié ?') }}
-                            </a>
-                        @endif
+                                @if (Route::has('password.request'))
+                                    <a class="btn btn-link" href="{{ route('password.request') }}">
+                                        {{ __('Mot de passe oublié ?') }}
+                                    </a>
+                                @endif
+                            </div>
+                        </form>
                     </div>
-                </div>
-            </form>
-
-
-
-
-
                 </div>
             </div>
         </div>
     </div>
-</div>
 @endsection
