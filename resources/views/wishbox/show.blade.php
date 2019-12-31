8eecbd1145c29f2ae7690727d47176263d63b224
Henri Joël Aïdasso <ahenrij@gmail.com>
2019-12-31 19:38:05 +0100

    wishes by categories fixed

--- resources/views/wishbox/show.blade.php
+++ resources/views/wishbox/show.blade.php
@@ -50,3 +50,74 @@
 
     </div>
 @endsection
+@section('additionalPageScripts')
+    <script src="{{ asset('js/imagesloaded.pkgd.min.js') }}"></script>
+    <script src="{{ asset('js/isotope.pkgd.min.js') }}"></script>
+
+    <script>
+      // Edit pagination links to get scroll after page reload by adding an anchor
+      // Wishes
+      var $links = $('#paginate-wishes li a');
+
+      var href='';
+      $links.each(function(){
+        href = $(this).attr('href');
+        $(this).attr('href', href+'#wishes');
+      });
+
+      // Gifts
+      $links = $('#paginate-gifts li a');
+
+      href='';
+      $links.each(function(){
+        href = $(this).attr('href');
+        $(this).attr('href', href+'#gifts');
+      });
+      //--
+      $(function() {
+          /* Isotope Gallery */
+
+        // init isotope
+        var $gallery = $(".tm-gallery").isotope({
+          itemSelector: ".tm-gallery-item",
+          layoutMode: "fitRows"
+        });
+        // layout Isotope after each image loads
+        $gallery.imagesLoaded().progress(function() {
+          $gallery.isotope("layout");
+        });
+
+        $(".filters-button-group").on("click", "a", function() {
+          var filterValue = $(this).attr("data-filter");
+          $gallery.isotope({ filter: filterValue });
+          console.log("Filter value: " + filterValue);
+        });
+
+          /* Tabs */
+        $(".tabgroup > div").hide();
+        $(".tabgroup > div:first-of-type").show();
+        $(".tabs a").click(function(e) {
+          e.preventDefault();
+          var $this = $(this),
+            tabgroup = "#" + $this.parents(".tabs").data("tabgroup"),
+            others = $this
+              .closest("li")
+              .siblings()
+              .children("a"),
+            target = $this.attr("href");
+          others.removeClass("active");
+          $this.addClass("active");
+
+          // Scroll to tab content (for mobile)
+          if ($(window).width() < 992) {
+            $("html, body").animate(
+              {
+                scrollTop: $("#tmGallery").offset().top
+              },
+              200
+            );
+          }
+        });
+      });
+    </script>
+@endsection
